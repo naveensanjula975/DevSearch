e19af56af4fb25d6dc02f6df243f7996a0d16c03
naveen sanjula <naveensanjula975@gmail.com>
2022-06-13 11:57:20 +0530

    <li>Title : {{ project.title }}</li>

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+projectsList = [
+    {
+        'id': '1',
+        'title': "Ecommerce Website",
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': "Portfolio Website",
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '3',
+        'title': "Social Network",
+        'description': 'Fully functional ecommerce website'
+    },
+
+]
+
 def projects(request):
     page = 'projects'
-    number = 10
-    context = {'page':page, 'number':number}
+    number = 10 
+    context = {'page':page, 'number':number, 'projects':projectsList}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
