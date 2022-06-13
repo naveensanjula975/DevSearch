83c93d74ef7c7c8a1ac1fb2277135d888096123f
naveen sanjula <naveensanjula975@gmail.com>
2022-06-13 09:31:39 +0530

    HttpResponse

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('Here are our products')
+
+def project(request, pk):
+    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects/', projects, name="projects"),
+    path('project/<str:pk>/', project, name="project"),
 ]
