fc03447c57e8801ddb45a9656fe63077457324f0
naveen sanjula <naveensanjula975@gmail.com>
2022-06-14 21:45:58 +0530

    def

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from .models import Project
 
 
-
 def projects(request):
     projects = Project.objects.all()
     context = { 'projects':projects}
@@ -12,3 +11,8 @@ def projects(request):
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj })
+
+
+def createProject(request):
+    context = {}
+    return render(request, "projects/project_form.html", context)
