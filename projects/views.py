adff69fbedf8785ca8c7cbcf59aa575b9f82ae2c
naveen sanjula <naveensanjula975@gmail.com>
2022-06-14 21:38:13 +0530

    remove path

--- projects/views.py
+++ projects/views.py
@@ -3,24 +3,6 @@ from django.http import HttpResponse
 from .models import Project
 
 
-projectsList = [
-    {
-        'id': '1',
-        'title': "Ecommerce Website",
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': "Portfolio Website",
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '3',
-        'title': "Social Network",
-        'description': 'Fully functional ecommerce website'
-    },
-
-]
 
 def projects(request):
     projects = Project.objects.all()
