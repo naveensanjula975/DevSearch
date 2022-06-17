944fc2d604c4f9fd377f7a4ef9ef5a5e12bedb3a
naveen sanjula <naveensanjula975@gmail.com>
2022-06-17 15:15:29 +0530

    media root

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -121,10 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Default primary key field type
