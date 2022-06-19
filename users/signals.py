f8e6146412b409b4a898e41826bea53787f61987
naveen sanjula <naveensanjula975@gmail.com>
2022-06-19 20:20:01 +0530

    import

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,12 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from django.contrib.auth.models import User
+from .models import Profile
+
+
 # @receiver(post_save, sender=Profile)
+
 def createProfile(sender, instance, created, **kwargs):
     if created:
         user = instance
