f0f43a4c661e7e6977f68f0157ce0e9fe7168895
naveen sanjula <naveensanjula975@gmail.com>
2022-06-19 21:33:16 +0530

    edit login

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 
-def loginPage(request):
+def loginUser(request):
 
     if request.method == 'POST':
         username = request.POST['username']
