e7c3c66ad905a6f4a2fceb56c6e51e07d8a33d48
naveen sanjula <naveensanjula975@gmail.com>
2022-06-19 21:43:53 +0530

    @login_required

--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Profile
 
 
 def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
 
     if request.method == 'POST':
         username = request.POST['username']
