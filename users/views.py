8c87a90fae6f2b3443c41615ed3f478382158c97
naveen sanjula <naveensanjula975@gmail.com>
2022-06-21 23:54:36 +0530

    return redirect

--- users/views.py
+++ users/views.py
@@ -90,12 +90,14 @@ def userAccount(request):
 @login_required(login_url='login')
 def editAccount(request):
     profile = request.user.profile
-    form = ProfileForm()
+    form = ProfileForm(instance=profile)
 
     if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILE, instance=profile)
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
-            
+
+            return redirect('account')
+
     context = {'form': form}
     return render(request, 'users/profile_form.html', context)
