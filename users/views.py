50dac7c4954f36a577786a7f495a875ea84b0100
naveen sanjula <naveensanjula975@gmail.com>
2022-06-21 16:40:08 +0530

    UserCreationForm

--- users/views.py
+++ users/views.py
@@ -1,12 +1,10 @@
-from email import message
-from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 
 def loginUser(request):
@@ -42,16 +40,23 @@ def logoutUser(request):
 
 def registerUser(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save() 
 
             messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('profiles')
+
+        else:
+            messages.success(request, 'An error has occurred during registration')
+
 
     context = {'page':page, 'form':form}
     return render(request, 'users/login_register.html', context)
