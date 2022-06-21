897c6c2106efa7116cc61c1aca034115a19fabeb
naveen sanjula <naveensanjula975@gmail.com>
2022-06-21 23:48:50 +0530

    import

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 
 def loginUser(request):
@@ -89,6 +89,13 @@ def userAccount(request):
 
 @login_required(login_url='login')
 def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm()
 
-    context = {}
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILE, instance=profile)
+        if form.is_valid():
+            form.save()
+            
+    context = {'form': form}
     return render(request, 'users/profile_form.html', context)
