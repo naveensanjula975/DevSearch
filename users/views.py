fcd53f68916f3534eb17a95ab5c8e1c67b159b16
naveen sanjula <naveensanjula975@gmail.com>
2022-06-22 18:29:16 +0530

    delete skill confirm btn

--- users/views.py
+++ users/views.py
@@ -142,6 +142,10 @@ def updateSkill(request, pk):
 def deleteSkill(request, pk):
     profile = request.user.profile
     skill = profile.skill_set.get(id=pk)
-    
+    if request.method == 'POST':
+        skill.delete()
+        messages.success(request, 'Skill was deleted successfully!')
+        return redirect('account')
+
     context = {'object': skill}
     return render(request, 'delete_template.html', context)
