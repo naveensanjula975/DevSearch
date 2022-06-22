5f91b7b7db4290230c00c6060b5c9fc3032ebb20
naveen sanjula <naveensanjula975@gmail.com>
2022-06-22 13:10:19 +0530

    User Project CRUD

--- projects/views.py
+++ projects/views.py
@@ -32,7 +32,8 @@ def createProject(request):
 
 @login_required(login_url="login")
 def updateProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
 
     if request.method =='POST':
@@ -46,7 +47,8 @@ def updateProject(request, pk):
 
 @login_required(login_url="login")
 def deleteProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     if request.method == 'POST':
         project.delete()
         return redirect('projects')
