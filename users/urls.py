fa0600c09f71991dbf3abb8a1131787d8eda9fb7
naveen sanjula <naveensanjula975@gmail.com>
2022-06-22 17:01:42 +0530

    update skill linked

--- users/urls.py
+++ users/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('edit-account/', views.editAccount, name="edit-account"),
 
     path('create-skill/', views.createSkill, name="create-skill"),
+    path('update-skill<str:pk>/', views.updateSkill, name="update-skill"),
 ]
