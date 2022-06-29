33152966968e5e90b89f3feede98137eb26260ce
naveen sanjula <naveensanjula975@gmail.com>
2022-06-29 11:10:03 +0530

    search_query

--- projects/views.py
+++ projects/views.py
@@ -1,25 +1,39 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Project
+from django.db.models import Q
+
+from .models import Project, Tag
 from .forms import ProjectForm
 
 
 def projects(request):
-    projects = Project.objects.all()
-    context = { 'projects':projects}
+    search_query = ''
+
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+
+    projects = Project.objects.filter(
+        Q(title__icontains=search_query) |
+        Q(description__icontains=search_query) |
+        Q(owner__name__icontains=search_query)
+    )
+
+    context = {'projects': projects, 'search_query': search_query}
     return render(request, 'projects/projects.html', context)
+
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    return render(request, 'projects/single-project.html', {'project': projectObj })
+    return render(request, 'projects/single-project.html', {'project': projectObj})
+
 
 @login_required(login_url="login")
 def createProject(request):
     profile = request.user.profile
     form = ProjectForm()
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             project = form.save(commit=False)
@@ -30,13 +44,14 @@ def createProject(request):
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
 
+
 @login_required(login_url="login")
 def updateProject(request, pk):
     profile = request.user.profile
     project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
@@ -44,6 +59,7 @@ def updateProject(request, pk):
 
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
+
 
 @login_required(login_url="login")
 def deleteProject(request, pk):
@@ -54,6 +70,3 @@ def deleteProject(request, pk):
         return redirect('projects')
     context = {'object': project}
     return render(request, 'delete_template.html', context)
-
-
-
