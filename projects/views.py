92bdbf095226919708c9b8e5b27dd5ab1759de19
naveen sanjula <naveensanjula975@gmail.com>
2022-07-01 00:12:22 +0530

    ReviewForm 2

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
+from django.core import paginator
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Project, Tag
 from .forms import ProjectForm, ReviewForm
 from .utils import searchProjects, paginationProjects
@@ -20,6 +22,17 @@ def projects(request):
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     form = ReviewForm()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+        #Update project vote count
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
+
     return render(request, 'projects/single-project.html', {'project': projectObj, 'form': form})
 
 
