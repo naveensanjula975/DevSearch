42bd76f667de5f9b2d8cc9e4dc9f5c58b590d550
naveen sanjula <naveensanjula975@gmail.com>
2022-07-02 01:05:49 +0530

    message.('Your message was successfully sent!')

--- users/views.py
+++ users/views.py
@@ -182,6 +182,27 @@ def viewMessage(request, pk):
 def createMessage(request, pk):
     recipient = Profile.objects.get(id=pk)
     form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+
+            message.success(request, 'Your message was successfully sent!')
+            return redirect('user-profile', pk=recipient.id)
+
     context = {'recipient': recipient, 'form': form}
     return render(request, 'users/message_form.html', context)
 
