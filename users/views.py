d02777dc3a4c4eef27e110748a422b6d5b72ca9e
naveen sanjula <naveensanjula975@gmail.com>
2022-07-01 23:29:41 +0530

    inbox 1

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
@@ -147,7 +148,7 @@ def updateSkill(request, pk):
     context = {'form': form}
     return render(request, 'users/skill_form.html', context)
 
-
+@login_required(login_url='login')
 def deleteSkill(request, pk):
     profile = request.user.profile
     skill = profile.skill_set.get(id=pk)
@@ -158,3 +159,9 @@ def deleteSkill(request, pk):
 
     context = {'object': skill}
     return render(request, 'delete_template.html', context)
+
+@login_required(login_url='login')
+def inbox(request):
+    context = {}
+    return render(request, 'users/inbox.html', context)
+
