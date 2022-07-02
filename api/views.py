b366e2a152c78c3d3d0ee74854425d514370c346
naveen sanjula <naveensanjula975@gmail.com>
2022-07-02 14:02:50 +0530

    # 1 Setting up a simple API

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from django.db import router
+from django.http import JsonResponse
+
+
+def getRoutes(request):
+
+    routes = [
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        {'POST': '/api/projects/id/vote'},
+
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refresh'},
+
+    ]
+    return JsonResponse(routes)
