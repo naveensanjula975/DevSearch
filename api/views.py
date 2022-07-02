9b8ee246ad30cb99e9909b5e1650532c64cac428
naveen sanjula <naveensanjula975@gmail.com>
2022-07-03 00:25:01 +0530

    Sending POST Requests

--- api/views.py
+++ api/views.py
@@ -2,9 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project
-
-from api import serializers
+from projects.models import Project, Review
 
 
 @api_view(['GET'])
@@ -17,13 +15,11 @@ def getRoutes(request):
 
         {'POST': '/api/users/token'},
         {'POST': '/api/users/token/refresh'},
-
     ]
     return Response(routes)
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
 def getProjects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
@@ -33,5 +29,25 @@ def getProjects(request):
 @api_view(['GET'])
 def getProject(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
     serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
