3dbc160579561bc354020b0d0e70749181910436
naveen sanjula <naveensanjula975@gmail.com>
2022-07-03 13:27:36 +0530

    createProject tags

--- projects/views.py
+++ projects/views.py
@@ -45,11 +45,16 @@ def createProject(request):
     form = ProjectForm()
 
     if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',',  " ").split()
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             project = form.save(commit=False)
-            profile.owner = profile
+            project.owner = profile
             project.save()
+
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
             return redirect('account')
 
     context = {'form': form}
